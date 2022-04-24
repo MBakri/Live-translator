f9a92abed412a44bbae3fc87ee2f58d184d76553
Malek Bakri <mbakri95@outlook.com>
2022-04-24 23:24:21 +0300

    deploy

--- src/controllers/audioprocessor.ts
+++ src/controllers/audioprocessor.ts
@@ -1,12 +1,8 @@
 import { Request, Response } from "express";
-import { Blob } from 'buffer';
-import { isInt32Array } from "util/types";
 import * as service from "../services/audioprocessor";
 export const translate = async (req: Request, res: Response, next: any) => {
-	//console.log(req);
 	try {
 		let data = await service.create(req.params.id, req.file?.originalname!);
-		console.log(data, 'DATA');
 		const view = new Int32Array(data);
 		res.send(view);//.json({ data: data });
 	} catch (e) {
